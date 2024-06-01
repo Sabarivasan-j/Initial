deafffe29b03e2be35633724bde60d2ce032f76d
Sabarivasan-j <sabarivasanj001@gmail.com>
2024-06-01 18:55:56 +0530

    java files

---
+++ Animal.java
@@ -0,0 +1,13 @@
+package TCS_PKG;
+import Infosys_PKG.Dog;
+public class Animal {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World Sabari!!!");
+		System.out.println(Infosys_PKG.Dog.name);
+		//Dog dog=new Dog();
+		System.out.println(Dog.dummy());
+		//Dog.main();
+	}
+
+}
